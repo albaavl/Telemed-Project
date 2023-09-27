7955e4ada5ce5898dd9942ea43003bd8cb71d1fb
albaavl <a.vargas10@usp.ceu.es>
2023-09-27 13:20:42 +0200

    refactor

--- Server/databaseManager.py
+++ Server/databaseManager.py
@@ -1,9 +1,11 @@
 import sqlite3
 
+
 def __init():
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
     if cursor.execute('SELECT name FROM sqlite_master').fetchone() is None:
         cursor.execute('CREATE TABLE patients(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)')
-        cursor.execute('CREATE TABLE reports(id INTEGER PRIMARY KEY AUTOINCREMENT, patient_id INTEGER REFERENCES patients(id) ON UPDATE CASCADE ON DELETE SET NULL, date DATE NOT NULL, symptoms TEXT, paramBitalino TEXT NOT NULL, HpComments TEXT)')
-
+        cursor.execute('CREATE TABLE reports(id INTEGER PRIMARY KEY AUTOINCREMENT, '
+                       'patient_id INTEGER REFERENCES patients(id) ON UPDATE CASCADE ON DELETE SET NULL,'
+                       'date DATE NOT NULL, symptoms TEXT, paramBitalino TEXT NOT NULL, HpComments TEXT)')
