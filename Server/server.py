35ac2bcaff1cd938deb5886f0dece5f3845b10d7
albaavl <a.vargas10@usp.ceu.es>
2023-09-28 10:50:49 +0200

    more server

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,4 @@
-import socket, select
+import socket, select, pickle
 
 class myServer:
     def __init__(self, ip_port=("0.0.0.0",1111)):
@@ -16,15 +16,39 @@ class myServer:
     def listen(self):
         while True:
             r_socket, _, _ = select.select(self.sockets[:], [], [])
-            for s in r_socket:
-                if s == self.sockets[0]:
+            for clientSocket in r_socket:
+                if clientSocket == self.sockets[0]:
                     self.accept_client()
                 else:
-
-                    # self.read_message(s)
+                    self.read_message(clientSocket)
 
     def accept_client(self):
         s_client, direction_client = self.sockets[0].accept()
         self.sockets.append(s_client)
         print("Connection opened")
+        print(direction_client)
 
+    def read_message(self, csocket):
+        message = csocket.recv(1024)
+        if not message:
+            self.disconnectClient(csocket)
+        else:
+            dic_message = pickle.loads(message)
+            print(dic_message)
+            if not dic_message:
+                print('Disconnected')
+                self.disconnectingClient(csocket)
+            else:
+                print('Message received')
+                self.decode_message(dic_message,csocket)
+
+    def decode_message(self,dic_message,csocket):
+        possible_controls = ['new_report','show_patients','show_reports','add_comments','add_user','delete_user','login']
+        if dic_message['control'] not in possible_controls:
+            csocket.send(pickle.dumps({'control': 'error', 'content':'Error: format not understood'}))
+        elif dic_message['control'] == 'new_report':
+            #TODO add report to database and error control
+            print('adding new report')
+            csocket.send(pickle.dumps({'control': 'success', 'content': 'Success: report added to database'}))
+        elif dic_message['control'] == 'show_patients':
+            #TODO database
