a8fb4c10b52bc1e22c28bcda864fc1f4233afe42
Sergio García Olaya <s.garcia169@usp.ceu.es>
2023-10-03 15:23:39 +0200

    added username and password db from hugo to dbman

--- Server/databaseManager.py
+++ Server/databaseManager.py
@@ -12,17 +12,38 @@ class Manager:
                         'patient_id INTEGER REFERENCES patients(id) ON UPDATE CASCADE ON DELETE SET NULL,'
                         'date DATE NOT NULL, symptoms TEXT, paramBitalino TEXT NOT NULL, HpComments TEXT)')
             
-    def createPatient(self, medicalId, name):
+    def createPatient(self, medicalId:int, name:str):
         self.cursor.execute('INSERT INTO patients (medicalId, name) VALUES ('+medicalId+', '+name+')')
 
-    def deletePatient(self, id):
+    def deletePatient(self, id:int):
         self.cursor.execute('DELETE FROM patients WHERE id = '+id)
 
-    def modifySymptoms(self, id, text):
+    def modifySymptoms(self, id:int, text:str):
         self.cursor.execute('UPDATE reports SET symptoms='+text+' WHERE patient_id='+id)
 
-    def modifyHpComments(self, id, text):
+    def modifyHpComments(self, id:int, text:str):
         self.cursor.execute('UPDATE reports SET HpComments='+text+' WHERE patient_id='+id)
 
-    def modifyparamBitalino(self, id, text):
+    def modifyparamBitalino(self, id:int, text:str):
         self.cursor.execute('UPDATE reports SET paramBitalino='+text+' WHERE patient_id='+id)
+
+    def setUp(self):
+        self.cursor.execute("CREATE TABLE users("+
+                       "userId INTEGER PRIMARY KEY ON DELETE CASCADE NOT NULL AUTOINCREMENT,"+
+                       "username TEXT NOT NULL,"+
+                       "userType TEXT NOT NULL,"+
+                       "password BLOB NOT NULL)")
+                    
+    def checkUser(self,username:str,password:bytes):
+        self.cursor.execute("SELECT * FROM users WHERE username=? AND password=? LIMIT 1",(username,password))
+        return self.cursor.fetchone()
+
+    
+    def createUser(self, username:str, password:bytes, userType:str):
+        '''Raises ValueError if userType is not admin, patient or clinician'''
+        if userType not in ("admin", "patient","clinician"): raise ValueError
+        self.cursor.execute("INSERT INTO users (userId, username, userType, password) VALUES (?,?,?)",(username,password,userType))
+        
+
+    def deleteUser(self, userId:int):
+        self.cursor.execute("DELETE * FROM users WHERE userId = ?",(userId,))
