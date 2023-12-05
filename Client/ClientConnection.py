e1addcc89a245c8dd6f03a0b0bce57063f668e23
hugop <h.perez10@usp.ceu.es>
2023-12-05 17:02:17 +0100

    Cli changes

--- Client/ClientConnection.py
+++ Client/ClientConnection.py
@@ -4,23 +4,23 @@ import socket
 class ClientConnection:
     '''Class containing client socket, any connection to server should be done through this class.'''
 
-    def __init__(self,servIp:str|None='10.60.59.104',servPort:int|None=1111):
+    def __init__(self,servIp:str|None='10.60.58.172',servPort:int|None=1111):
         self.socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.connect((servIp,servPort))
+        self.socket.connect((servIp,servPort)) 
     
     def sendMsg(self,msg:bytes):
         '''Sends the provided bytes to the server'''
-        self.socket.send(msg)
+        self.socket.sendall(msg)
 
-    def recvMsg(self,buffSize:int):
+    def recvMsg(self):
         '''Returns bytes received from server'''
 
         fBytes=b''
         while True:
-            rBytes=self.socket.recv(buffSize)
+            rBytes=self.socket.recv(1024)
             fBytes+=rBytes
-            if(len(rBytes)<buffSize): return fBytes
+            if(len(rBytes)<1024): return fBytes
         
 
     def logOut(self):
--- Client/clientLogic.py
+++ Client/clientLogic.py
@@ -36,12 +36,12 @@ def decodeServerResponse(query:bytes) -> bytes:
 
 #Patient Only    
 
-def patient_connectToBitalino(mac:str = '98:d3:11:fd:1e:cc', running_time = 60):
+def patient_connectToBitalino(mac:str = '98:d3:11:fd:1e:cc', running_time = 15):
     try:
         acqChannels = [1]  # channel 2 pero array empieza en 0
-        samplingRate = 1000
+        samplingRate = 10
         nSamples = 100
-        digitalOutput_on = [1, 1]
+        digitalOutput_on = [1, 0]
         digitalOutput_off = [0, 0]
         # Connect to BITalino
         device = bit(mac)
